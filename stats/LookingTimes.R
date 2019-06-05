0a82e2043257596083219112e3c471812f4cee25
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2019-06-05 07:47:52 +0100

    contrast_trials plots generated (one plot per salience_diff)

--- stats/LookingTimes.R
+++ stats/LookingTimes.R
@@ -36,3 +36,37 @@ ggsave(paste0(save_path, "Global_data.pdf"),
        dpi = 600)
 
 # CONTRAST TRIALS ==================================================================================
+save_path <- "../results/ContrastTrials/"
+
+contrast_trials <- contrast_trials %>%
+  spread(feature, looking_time) %>%
+  mutate(novelty_pref = New / (Old + New))
+
+contrast_trials.plots <- contrast_trials %>%
+  split(.$salience_diff) %>%
+  lapply(function(df){
+    p <- ggplot(df,
+                aes(x = condition,
+                    y = novelty_pref,
+                    colour = condition,
+                    fill = condition)) +
+      theme_bw() + ylab("Looking to New Feature (Prop)") + ylim(0,1) +
+      geom_hline(yintercept = .5, colour = "black", linetype = 2) +
+      theme(legend.position = "top",
+            axis.title.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            axis.text.y = element_blank()) +
+      coord_flip() + facet_grid(.~contrast_type) +
+      geom_flat_violin(position = position_nudge(x = .2),
+                       colour = "black", alpha = .5, width = .7) +
+      geom_point(position = position_jitter(width = .15, height = .005),
+                 size = 1, alpha = .6,
+                 show.legend = F) +
+      geom_boxplot(width = .1, alpha = .3, outlier.shape = NA, colour = "black",
+                   show.legend = F) +
+      scale_color_brewer(palette = "Dark2") +
+      scale_fill_brewer(palette = "Dark2")
+    ggsave(paste0(save_path, "SalienceDiff", first(df$salience_diff),"_data.pdf"), p,
+           width = 5.5, height = 3, dpi = 600)
+    return(p)
+  })
